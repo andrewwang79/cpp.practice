f100a76de9700229a151e08ada80cc803e8e2f73
andrewwang79 <andrewwang79@gmail.com>
2021-05-25 17:04:58 +0800

    no message

--- log/main.cpp
+++ log/main.cpp
@@ -1,10 +1,3 @@
-// widget.cpp: 定义应用程序的入口点。
-//
-
-#include <iomanip>
-#include <iostream>
-#include <thread>
-
 #include "Logger.h"
 
 using namespace std;
@@ -13,11 +6,15 @@ using namespace log4cplus;
 IMPLEMENT_LOGGER(main)
 
 int main() {
+  // 初始化
   log4cplus::PropertyConfigurator::doConfigure(LOG4CPLUS_STRING_TO_TSTRING("./conf/logger.cfg"));
+
+  // 示例
   LOG_INFO("log demo. Hello/你好, World. This is log demo.");
   string code = "111";
   LOG_DEBUG_FMT("result: code[%s], length[%d]", code.c_str(), 100);
   LOG_INFO("press any key to quit.");
+
   getchar();
   return 0;
 }
