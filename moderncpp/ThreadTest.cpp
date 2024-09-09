873a71d47606ff72d127aa716cd6f7909375b466
andrewwang79 <andrewwang79@gmail.com>
2024-09-09 09:39:45 +0800

    trivial

--- moderncpp/ThreadTest.cpp
+++ moderncpp/ThreadTest.cpp
@@ -10,15 +10,13 @@ bool thread2_done = false;
 bool stop_threads = false;
 
 void thread_function(int id) {
-  std::this_thread::sleep_for(std::chrono::seconds(id));  // 模拟不同的工作时间
+  if (id == 1) {
+    thread1_done = true;
+  } else {
+    thread2_done = true;
+  }
   {
-    if (id == 1) {
-      thread1_done = true;
-      //   std::this_thread::sleep_for(std::chrono::seconds(7));
-    } else {
-      thread2_done = true;
-    }
-    std::lock_guard<std::mutex> lock(mtx);
+    std::lock_guard<std::mutex> lock(mtx);  // 减少锁的范围和影响
     std::cout << "Thread " << id << " lock." << std::endl;
     stop_threads = true;
     std::cout << "Thread " << id << " done." << std::endl;
@@ -35,17 +33,18 @@ void main_thread_function() {
 
   std::unique_lock<std::mutex> lock(mtx);  // 构造锁定mtx，析构释放mtx
   std::cout << "Main thread lock." << std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(6));
+  // std::this_thread::sleep_for(std::chrono::seconds(6));
   std::cout << "Main thread weak." << std::endl;
   cv.wait(lock, [] { return stop_threads; });  // 等待任意一个线程结束
   std::cout << "Main thread detected one thread finished, stopping both threads." << thread1_done << "|" << thread2_done << std::endl;
 }
 
 void cv_test() {
+  // 验证：thread_function如果没有锁保护，且cv.notify_all()通知在主线程cv.wait()之前发生，会导致wait()一直阻塞。但实践中没有出来此情况
   std::thread t1(thread_function, 1);
   std::thread t2(thread_function, 2);
 
-  main_thread_function();  // 主线程等待
+  main_thread_function();
 
   if (t1.joinable()) t1.join();
   if (t2.joinable()) t2.join();
